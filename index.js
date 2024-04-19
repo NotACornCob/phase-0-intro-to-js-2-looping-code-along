28839803c65667ebf96505ed09ccefc3ddd58773
Alex <alex.kleinman@gmail.com>
2024-04-19 18:17:49 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const recipient = ["charlieCard", "samipCard", "aliCard"];
+const cardType = "birthday";
+const cardArray = []
+
+function writeCards(recipient, cardType) { 
+    for (let i = 0; i < recipient.length; i++) { 
+        cardArray.push(`Thank you, ${recipient[i]}, for the wonderful ${cardType} gift!`)
+    }
+    return cardArray;
+}
+
+
+function countDown(x) {
+    while (x > -1) { ;
+    console.log(x--)
+    }
+}
+
